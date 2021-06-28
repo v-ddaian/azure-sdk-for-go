e7c495cfa5ccd924580d698f12664e6f567a7b4f
Dragos Daian <v-ddaian@microsoft.com>
2021-06-28 14:14:37 +0200

    add samples.

---
+++ services/preview/maps/2.0/creator/go.mod
@@ -0,0 +1,12 @@
+module creator
+
+go 1.16
+
+require (
+	github.com/Azure/azure-sdk-for-go v55.4.0+incompatible
+	github.com/Azure/go-autorest/autorest v0.11.19
+	github.com/Azure/go-autorest/autorest/date v0.3.0
+	github.com/Azure/go-autorest/autorest/to v0.4.0
+	github.com/Azure/go-autorest/autorest/validation v0.3.1
+	github.com/Azure/go-autorest/tracing v0.6.0
+)
---
+++ services/preview/maps/2.0/samples/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"creator"
+
+
+)
+
+func main() {
+	creator.New("a")
+	//creator.New("a")
+	//client := creator.NewRouteClient(os.Getenv("SUBSCRIPTION_KEY"), "")
+	//response, err := client.GetRouteDirections(context.Background(), azmaps.JSON, "52.50931,13.42936:52.50274,13.43872", nil, "", nil, nil, nil, nil, azmaps.Text, "en", nil, azmaps.RouteRepresentationPolyline, azmaps.All, nil, "", azmaps.SectionTypeCarTrain, nil, nil, nil, nil, nil, nil, nil, azmaps.WindingnessLevelHigh, azmaps.High, azmaps.TravelModeBicycle, []azmaps.RouteAvoidType{azmaps.AlreadyUsedRoads, azmaps.BorderCrossings}, nil, azmaps.Thrilling, azmaps.OtherHazmatExplosive, azmaps.Combustion, nil, nil, nil, nil, nil, nil, nil, nil, "", "", "", "")
+	//if err == nil {
+	//	print(response.Response.Status)
+	//} else {
+	//	print(err.Error())
+	//}
+}
